d23038ca34139e216d1d0670c1bf255321f20b88
Tibor Bodecs <mail.tib@gmail.com>
2024-03-21 10:11:46 +0100

    add key generator

---
+++ Tests/DatabaseKitTests/KeyGenerationTests.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//
+//
+//  Created by Tibor Bodecs on 21/03/2024.
+//
+
+import DatabaseMigrationKit
+import DatabaseQueryKit
+import FeatherComponent
+import FeatherRelationalDatabase
+import NanoID
+import XCTest
+
+final class KeyGenerationTests: TestCase {
+
+    func testKeyGeneration() {
+        struct MyModel {}
+
+        let nanoId: Key<MyModel> = NanoID.generateKey()
+
+        XCTAssertEqual(nanoId.rawValue.count, 21)
+
+        let customId: Key<MyModel> = UUID.generateKey()
+        let customId2: Key<MyModel> = .init(rawValue: customId.rawValue)
+
+        XCTAssertEqual(customId, customId2)
+    }
+}
