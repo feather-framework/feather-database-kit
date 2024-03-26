6b7041920f73da746d6594b73773094fb29c5beb
Tibor Bodecs <mail.tib@gmail.com>
2024-03-26 20:10:48 +0100

    fix api script

--- scripts/check-api-breakage.sh
+++ scripts/check-api-breakage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu
+set -euo pipefail
 
 log() { printf -- "** %s\n" "$*" >&2; }
 error() { printf -- "** ERROR: %s\n" "$*" >&2; }
@@ -10,11 +10,10 @@ REPO_ROOT="$(git -C "${CURRENT_SCRIPT_DIR}" rev-parse --show-toplevel)"
 
 git fetch -t 
 LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
-#swift package diagnose-api-breaking-changes "$LATEST_TAG"
-swift package diagnose-api-breaking-changes 0.7.0 2>&1 > api.log
-cat api.log
+# swift package diagnose-api-breaking-changes "$LATEST_TAG"
+swift package diagnose-api-breaking-changes 0.7.0 2>&1 > api-breakages.log || { 
+    NUM=$(cat api-breakages.log|grep "💔"|wc -l)
+    fatal "❌ Found ${NUM} API breakages."
+}
 
-# if [ "${NUM_BROKEN_SYMLINKS}" -gt 0 ]; then
-#   fatal "❌ Found ${NUM_BROKEN_SYMLINKS} symlinks."
-# fi
-#log "✅ Found no api breakage."
+log "✅ Found no API breakages."
