8e1635d5cb0c0642af693329662cef46f431fddf
Tibor Bodecs <mail.tib@gmail.com>
2024-03-26 18:44:50 +0100

    api log

--- scripts/check-api-breakage.sh
+++ scripts/check-api-breakage.sh
@@ -11,7 +11,8 @@ REPO_ROOT="$(git -C "${CURRENT_SCRIPT_DIR}" rev-parse --show-toplevel)"
 git fetch -t 
 LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 #swift package diagnose-api-breaking-changes "$LATEST_TAG"
-swift package diagnose-api-breaking-changes 0.7.0 2>&1
+swift package diagnose-api-breaking-changes 0.7.0 2>&1 > api.log
+cat api.log
 
 # if [ "${NUM_BROKEN_SYMLINKS}" -gt 0 ]; then
 #   fatal "❌ Found ${NUM_BROKEN_SYMLINKS} symlinks."
